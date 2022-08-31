033033cb670072c8826fa1bbe7da350101c7409a
MagomedFR <magomed.khaptukaev@gmail.com>
2022-08-31 11:49:39 +0300

    Научил воевать

--- soldier.js
+++ soldier.js
@@ -1,13 +1,32 @@
-let solider = {
+let soldier = {
     name: 'Chuck Norris',
     health: 10,
     weapon: {
         weaponName: 'Automatic rifle',
-        патроны: 30,
+        bullets: 30,
     },
     supplies: 3,
 
-    shoot: function () {},
-    recharge: function () {},
-    hurt: function () {},
+    shoot: function () {
+        this.weapon.bullets--
+        console.log('бах-бах')
+        if(this.weapon.bullets == 0) {
+            console.log('обойма пуста. Перезаредитесь')
+        }
+    },
+    recharge: function () {
+        if(this.weapon.bullets < 30) {
+            this.weapon.bullets = 30
+            this.supplies--
+            console.log('перезарядка..')
+        }if (this.supplies == 0) {
+            console.log('не осталось припасов')
+        }
+    },
+    hurt: function () {
+        this.health--
+        if(this.health <= 0) {
+            console.log('Ты проиграл')
+        }
+    },
 }
