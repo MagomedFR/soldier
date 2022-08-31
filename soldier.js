58338bad231b6e0daf4afbfa60cc001c287d8262
MagomedFR <magomed.khaptukaev@gmail.com>
2022-08-31 11:56:04 +0300

    Все проверил

--- soldier.js
+++ soldier.js
@@ -1,32 +1,33 @@
 let soldier = {
-    name: 'Chuck Norris',
-    health: 10,
-    weapon: {
-        weaponName: 'Automatic rifle',
-        bullets: 30,
-    },
-    supplies: 3,
+  name: "Chuck Norris",
+  health: 10,
+  weapon: {
+    weaponName: "Automatic rifle",
+    bullets: 30,
+  },
+  supplies: 3,
 
-    shoot: function () {
-        this.weapon.bullets--
-        console.log('бах-бах')
-        if(this.weapon.bullets == 0) {
-            console.log('обойма пуста. Перезаредитесь')
-        }
-    },
-    recharge: function () {
-        if(this.weapon.bullets < 30) {
-            this.weapon.bullets = 30
-            this.supplies--
-            console.log('перезарядка..')
-        }if (this.supplies == 0) {
-            console.log('не осталось припасов')
-        }
-    },
-    hurt: function () {
-        this.health--
-        if(this.health <= 0) {
-            console.log('Ты проиграл')
-        }
-    },
-}
+  shoot: function () {
+    this.weapon.bullets--;
+    console.log("бах-бах");
+    if (this.weapon.bullets == 0) {
+      console.log("обойма пуста. Перезаредитесь");
+    }
+  },
+  recharge: function () {
+    if (this.weapon.bullets < 30) {
+      this.weapon.bullets = 30;
+      this.supplies--;
+      console.log("перезарядка..");
+    }
+    if (this.supplies == 0) {
+      console.log("не осталось припасов");
+    }
+  },
+  hurt: function () {
+    this.health--;
+    if (this.health <= 0) {
+      console.log("Ты проиграл");
+    }
+  },
+};
