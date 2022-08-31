e778bd6aaced613337cf38cb5cfb4ba0d6281b30
MagomedFR <magomed.khaptukaev@gmail.com>
2022-08-31 10:39:25 +0300

    Создал киборг-убийца

---
+++ soldier.js
@@ -0,0 +1,13 @@
+let solider = {
+    name: 'Chuck Norris',
+    health: 10,
+    weapon: {
+        weaponName: 'Automatic rifle',
+        патроны: 30,
+    },
+    supplies: 3,
+
+    shoot: function () {},
+    recharge: function () {},
+    hurt: function () {},
+}
